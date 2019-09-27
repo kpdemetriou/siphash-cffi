08f0ccbb11f773567bbce643061d5abd53728dee
Kyle Altendorf <sda@fstab.net>
2019-09-27 01:35:44 -0400

    fix pypy version list

--- ci/matrix.py
+++ ci/matrix.py
@@ -32,7 +32,7 @@ all_interpreters = tuple(interpreters.keys())
 
 versions = collections.OrderedDict((
     ('CPython', ('3.5', '3.6', '3.7')),
-    ('PyPy', ('3.5')),
+    ('PyPy', ('3.5',)),
 ))
 
 
